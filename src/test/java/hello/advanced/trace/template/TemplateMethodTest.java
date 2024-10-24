d2ee3abb52d1d115d46c39151d8ccb31f7c66c33
DoHunee <jehgns1@egmail.com>
2024-10-24 16:52:14 +0900

    템플릿 메서드 패턴 - 예제3

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -40,10 +40,33 @@ public class TemplateMethodTest {
      */
     @Test
     void templateMethodV1() {
-    AbstractTemplate template1 = new SubClassLogic1();
-    template1.execute();
-    AbstractTemplate template2 = new SubClassLogic2();
-    template2.execute();
+        AbstractTemplate template1 = new SubClassLogic1();
+        template1.execute();
+        AbstractTemplate template2 = new SubClassLogic2();
+        template2.execute();
+    }
+
+    /**
+     * 템플릿 메서드 패턴, 익명 내부 클래스 사용
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
     }
 
 }
