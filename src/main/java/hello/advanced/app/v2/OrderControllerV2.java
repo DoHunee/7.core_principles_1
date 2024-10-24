cfd3ca852e4cb7a147f5ad125891e2aaeee155fe
DoHunee <jehgns1@egmail.com>
2024-10-24 11:20:06 +0900

    로그 추적기 V2 - 적용

---
+++ src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    // http://localhost:8080/v2/request?itemId=hello
+    // http://localhost:8080/v2/request?itemId=ex
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
